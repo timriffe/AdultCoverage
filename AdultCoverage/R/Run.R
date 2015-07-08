a4dffda3b93c01f5c33a65c4fa664bc67f6fe1f1
tim riffe <tim.riffe@gmail.com>
2015-07-08 16:50:17 +0200

    fixed code and final results generation

--- AdultCoverage/R/Run.R
+++ AdultCoverage/R/Run.R
@@ -3,22 +3,34 @@ if (system("hostname",intern=TRUE) %in% c("triffe-N80Vm", "tim-ThinkPad-L440")){
 	# if I'm on the laptop
 	setwd("/home/tim/git/AdultCoverage/AdultCoverage")
 } else {
-	# in that case I'm on Berkeley system, and other people in the dept can run this too
-	setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/AdultCoverage/AdultCoverage"))
+	if (system("hostname",intern=TRUE) == "PC-403478"){
+		# on MPIDR PC
+		setwd("U://git//AdultCoverage//AdultCoverage")
+	} else {
+		# in that case I'm on Berkeley system, and other people in the dept can run this too
+		setwd(paste0("/data/commons/",system("whoami",intern=TRUE),"/git/AdultCoverage/AdultCoverage"))
+	}
 }
+getwd()
 
 ITA <- read.table("Data/italy.test.txt", 
   header = TRUE, sep = "\t", stringsAsFactors = FALSE)
-head(ITA)
+
 #Data in frame : cod, age, pop1, year1, pop2, year2, death (mean of two periods)
-x <- with(ITA, data.frame(
-    cod = Year, 
-    age = Age, 
-    pop1 = Pop.fem, 
-    year1 = Year, 
-    pop2 = Pop.fem.1, 
-    year2 = Year.1, 
-    death = (Death.fem + Death.fem.1)/2) )
-x$age[x$age == 4] <- 1
-# coerce to required format:
+
+# new data for Brasil to run. By regions.
+BR1 <- read.table(file.path("Data","data_Brazil_p1.txt"), 
+		header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+BR2 <- read.table(file.path("Data","data_Brazil_p2.txt"), 
+		header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+BR3 <- read.table(file.path("Data","data_Brazil_p3.txt"), 
+		header = TRUE, sep = "\t", stringsAsFactors = FALSE)
+
+
+
+
+
+
+
+
 
