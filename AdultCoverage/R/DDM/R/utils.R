f0cf48de5cfa4aaaf753deea108cb0a048218f29
Tim Riffe <tim.riffe@gmail.com>
2016-05-27 15:41:24 +0200

    add some notecode stuff

--- AdultCoverage/R/DDM/R/utils.R
+++ AdultCoverage/R/DDM/R/utils.R
@@ -13,9 +13,8 @@
 #' @param MinAge integer ignore ages above this age.
 #' @return integer the age interval. \code{NA} if this is not unique.
 #' @export
+#' 
 
-
-args(grepl)
 detectAgeInterval <- function(Dat, MinAge = 5, MaxAge = 70, ageColumn = "Age"){
 	
 	stopifnot("age" %in% towlower(colnames(Dat)))
@@ -29,3 +28,77 @@ if ()
 	}
 	Interval
 }
+
+
+#' @title a cheap way to choose which column to assign a NoteCode to
+#' 
+#' @description One property of the LexisDB scripts that might be useful for downstream checks is the ability to trace which functions have modified a given data object. These can go into NoteCode slots. This function writes \code{code} to the first unoccupied \code{NoteCode} column. If all three \code{NoteCode} columns are occupied, it concatenates the end of the third column. This way we preserve a full history. Unfortunately it gets split between columns. Oh well. Good for eyeballing. This function written for the sake of modularity. Function copied from HMD collection directly as-is.
+#' 
+#' @param X the HMD data object that presumably has three \code{NoteCode} columns
+#' @param code character string to assign to the column, typically the name of the function operating on \code{X}.
+#' 
+#' @export
+#' 
+
+assignNoteCode <- function(X, code){
+	
+	Free <- colSums(is.na(as.matrix(X[,c("NoteCode1","NoteCode2","NoteCode3")]))) > 0
+	if (any(Free)){
+		NoteCol <- paste0("NoteCode",min(which(Free)))
+		X[[NoteCol]] <- code
+	} else {
+		X$NoteCode3 <- paste(X$NoteCode3, code, sep = " ")
+	}
+	X
+}
+
+
+#' Logical utility functions
+#'
+#' @aliases logic HMDutils
+#' 
+#' @description These logical functions are like the usual ones, but \code{NA} values are treated as \code{FALSE} by default. This is not an exhaustive list, but these are the ones that speed our coding, and reduce code clutter. Functions copied from HMD collection directly as-is.
+#' 
+#' @param x,y any two vector that can be logically compared.
+#' @name HMDlogic
+#' 
+#' @examples
+#' \dontrun{
+#' c(1,2,NA,4,5) == c(1,NA,3,4,NA)
+#' # compare
+#' c(1,2,NA,4,5) %==% c(1,NA,3,4,NA)
+#' }
+NULL
+#' 
+
+#' @rdname HMDlogic
+'%==%' <- function(x,y){
+	x == y & !is.na(x) & !is.na(y)
+}
+
+#' @rdname HMDlogic
+'%!=%' <- function(x,y){
+	x != y & !is.na(x) & !is.na(y)
+}
+
+# note this is incompatible with magrittr!
+#' @rdname HMDlogic
+'%>%' <- function(x,y){
+	x > y & !is.na(x) & !is.na(y)
+}
+
+#' @rdname HMDlogic
+'%<%' <- function(x,y){
+	x < y & !is.na(x) & !is.na(y)
+}
+
+#' @rdname HMDlogic
+'%>=%' <- function(x,y){
+	x >= y & !is.na(x) & !is.na(y)
+}
+
+#' @rdname HMDlogic
+'%<=%' <- function(x,y){
+	x <= y & !is.na(x) & !is.na(y)
+}
+
